df2cd2404aa8940f8f53829b346de79df4cc032f
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-09-03 19:14:07 +0530

    feat: vim install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+sudo apt install curl
 mkdir -p ~/.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 rm ~/.bashrc
 cp ./.bashrc ~/.bashrc
 cp ./.vimrc ~/.vimrc
