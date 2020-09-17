8483c864e65fe206f759821d483c25bb6c507296
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-09-17 22:52:20 +0530

    nvim: config file

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 sudo apt install curl
 mkdir -p ~/.vim
+mkdir -p ~/.config/nvim
+
 if [ ! -f ~/.vim/autoload/plug.vim ]; then
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -10,7 +12,7 @@ rm ~/.bashrc
 cp ./.bashrc ~/.bashrc
 cp ./.vimrc ~/.vimrc
 cp ./.bash_aliases ~/.bash_aliases
-
+cp ~/init.vim ~/.config/nvim/init.vim
 
 echo "Change Download a NERD-AWESOME font and install it and change the default font"
 echo "Bash font size 13"
